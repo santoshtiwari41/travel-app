ae80372c9dcb7b6cf101490fa77f19746bd16d66
santoshtiwari41 <santoshhh066@gmail.com>
2026-01-04 11:07:59 +0545

    auth and app route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,12 +11,12 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-
+  
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      <Stack screenOptions={{ headerShown: false }} >
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="(auth)" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Redirect } from 'expo-router';
+import React from 'react';
+
+const index = () => {
+    const isAuthenticated = false;
+    if (!isAuthenticated) {
+        return <Redirect href="/(auth)/login" />;
+    }
+    else{
+        return <Redirect href="/(tabs)/home" />;
+    }
+    
+}
+
+export default index
