d9033a87b1201136f3646c4f67a8dc75e606e661
santoshtiwari41 <santoshhh066@gmail.com>
2026-02-04 12:30:53 +0545

    fix:logout ,api client error

--- api/client.ts
+++ api/client.ts
@@ -1,15 +1,17 @@
 import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
+import { io } from "socket.io-client";
 
 export const apiClient = axios.create({
-  baseURL: 'http://192.168.0.11:8080/api/v1',
+  baseURL: 'http://192.168.0.12:8000/api/v1',
   timeout: 10000,
 });
+export const socketClient=
 
 apiClient.interceptors.request.use(
   async (config) => {
     try {
-      const token = await SecureStore.getItemAsync('userToken');
+      const token = await SecureStore.getItemAsync('auth-token');
 
       if (token) {
         config.headers.Authorization = `Bearer ${token}`;
@@ -25,3 +27,9 @@ apiClient.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+
+export const socket = io("http://192.168.0.12:3000/api/v1", {
+  transports: ["websocket"], 
+  autoConnect: false,
+});
--- app/(main)/stack/profile/index.tsx
+++ app/(main)/stack/profile/index.tsx
@@ -2,12 +2,16 @@ import { View } from 'react-native'
 import React from 'react'
 import { Button } from '@/components/ui/button'
 import { useRouter } from 'expo-router'
-
+import * as SecureStore from 'expo-secure-store';
 const Profile = () => {
   const router=useRouter();
+  const handleLogut=async()=>{
+   await SecureStore.deleteItemAsync('auth-token');
+   router.replace('/(auth)/login')
+  }
   return (
     <View className='flex-1 justify-center align-center'>
-     <Button title="Log out" onPress={() => {router.replace('/login')}} variant="destructive" />
+     <Button title="Log out" onPress={handleLogut} variant="destructive" />
     </View>
   )
 }
